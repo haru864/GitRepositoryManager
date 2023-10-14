13d1cc61a7ec142ec646566b95fb1179960fa147
haru <kawachigenji95@gmail.com>
2023-10-15 01:48:59 +0900

    Implement "check" option.

--- src/functions.cpp
+++ src/functions.cpp
@@ -76,7 +76,8 @@ void check() noexcept
 
         FILE *inFile = fopen(scanResultFileAbsPath.c_str(), "r");
         char buffer[BUFFER_SIZE];
-        vector<string> repoListWithUnpushedChange = {};
+        vector<string> repogitoriesNotDifferFromRemote = {};
+        vector<string> repogitoriesDifferFromRemote = {};
 
         while (fgets(buffer, BUFFER_SIZE, inFile) != NULL)
         {
@@ -101,7 +102,6 @@ void check() noexcept
                               GIT_STATUS_OPT_RENAMES_HEAD_TO_INDEX |
                               GIT_STATUS_OPT_SORT_CASE_SENSITIVELY;
 
-            // ステータスの取得
             error = git_status_list_new(&status, repo, &statusopt);
             if (error != 0)
             {
@@ -111,48 +111,30 @@ void check() noexcept
                 exit(error);
             }
 
-            // ステータスの数を取得
-            size_t maxi = git_status_list_entrycount(status);
-            cout << pathWithoutLF << ", " << maxi << endl;
-            if (maxi > 0)
+            size_t numOfDiff = git_status_list_entrycount(status);
+            cout << pathWithoutLF << ", " << numOfDiff << endl;
+            if (numOfDiff > 0)
             {
-                repoListWithUnpushedChange.push_back(pathWithoutLF);
+                repogitoriesNotDifferFromRemote.push_back(pathWithoutLF);
             }
-            const git_status_entry *s;
-            for (size_t i = 0; i < maxi; ++i)
+            else
             {
-                s = git_status_byindex(status, i);
-                if (s->status == GIT_STATUS_CURRENT)
-                {
-                    continue;
-                }
-
-                // ここでステータス情報(s)を使って何か処理を行う
-                // 例えば、変更されたファイルのパスを出力する
-                if (s->head_to_index)
-                {
-                    cout << "Modified: " << s->head_to_index->old_file.path << endl;
-                }
-                else if (s->index_to_workdir)
-                {
-                    cout << "Untracked: " << s->index_to_workdir->old_file.path << endl;
-                }
+                repogitoriesDifferFromRemote.push_back(pathWithoutLF);
             }
 
             git_status_list_free(status);
             git_repository_free(repo);
         }
 
-        json j;
-        // json j["repogitory_with_unpushed_changes"] = repoListWithUnpushedChange;
-        j["list"] = {1, 0, 2};
-        // FILE *outFile = fopen(checkResultFileAbsPath.c_str(), "w");
-        // fprintf(outFile, j.get<string>().c_str());
         fclose(inFile);
-        // fclose(outFile);
+
+        json j;
+        j["repogitory_not_doffer_from_remote"] = repogitoriesNotDifferFromRemote;
+        j["repogitory_doffer_from_remote"] = repogitoriesDifferFromRemote;
 
         ofstream file(checkResultFileAbsPath);
         file << j << endl;
+        file.close();
 
         git_libgit2_shutdown();
     }
@@ -164,9 +146,5 @@ void check() noexcept
 }
 
 void list() noexcept
-{
-}
-
-void all(const string dirPath) noexcept
 {
 }
